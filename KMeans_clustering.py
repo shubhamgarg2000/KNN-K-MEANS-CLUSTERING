1f29b2440db2208c9c8f0bdcbfd28f6d2dda7cd4
Shubham garg <42842217+shubhamgarg2000@users.noreply.github.com>
2019-07-29 23:35:48 +0530

    Add files via upload

---
+++ KMeans_clustering.py
@@ -0,0 +1,42 @@
+#K-MEANS CLUSTERING
+
+#IMPORTING LIBRARIES
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#IMPORTING THE DATASET
+dataset=pd.read_csv('Mall_Customers.csv')
+x=dataset.iloc[:,[3,4]].values  #independent variable x
+y=dataset.iloc[:,3].values
+
+#SPLITTING THE DATASETINTO TRAINING+TEST
+from sklearn.cluster imporpt KMeans
+wcss=[]
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i,init='k-means++',random_state=42)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('THE ELBOW METHOD')
+plt.xlabel('number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#FITTING KMEANS TO DATASET
+
+kmeans=KMeans(n_clusters=5,init='k-means++',random_state=42)
+y_kmeans=kmeans.fit_predict(x)
+
+#VISUALISING CLUSTERS
+plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='red',label='cluster1')
+plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='cluster2')
+plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='cluster3')
+plt.scatter(x[y_kmeans==3,0],x[y_kmeans==3,1],s=100,c='cyan',label='cluster4')
+plt.scatter(x[y_kmeans==4,0],x[y_kmeans==4,1],s=100,c='magenta',label='cluster5')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow',label='centroids')
+plt.title('CLUSTER OF CUSTOMERS')
+plt.xlabel('ANNUAL INCOME')
+plt.ylabel('SPENDING SCORE')
+plt.legend()
+plt.show()
---
+++ KNN.py
@@ -0,0 +1,85 @@
+#we have to predict class
+'''
+1.IMPORT DATASET
+1.B PREPROCESSING
+2.SPLIT 
+3.TRAINING +TEST
+4.X_TRAIN,Y_TRAIN
+5.MODEL
+6.ACCESS -CONFUSION MATRIX
+7.PLOT GRAPH
+p=1 manhattan
+p=2 minkowski
+
+'''
+#K-NEAREST NEIGHBOURS
+
+#IMPORTING LIBRARIES
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#IMPORTING THE DATASET
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,[2,3]].values  #independent variable x
+y=dataset.iloc[:,4].values
+
+#SPLITTING THE DATASETINTO TRAINING+TEST
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+# FEATURE SCALING
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+#FITTING KNN TO TRAINING SET
+from sklearn.neighbors import KNeighborsClassifier
+classifier=KNeighborsClassifier(n_neighbors=7,metric='minkowski',p=2)
+classifier.fit(X_train,y_train)
+
+#PREDICTING THE TEST
+y_pred=classifier.predict(X_test)
+
+#MAKING CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+
+#VISUALISING TRAINING SET RESULTS
+from matplotlib.colors import ListedColormap
+X_set,y_set=X_train,y_train
+X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
+                  np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75,cmap=ListedColormap(('red','green')))
+#ravel function do flattening of array
+#.T is for transfering into column
+#alpha is used for intensity variation lowe alpha lower intensity
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j,0],X_set[y_set==j,1],
+                c=ListedColormap(('red','green'))(i),label=j)
+plt.title('K-NN training set')
+plt.xlabel('AGE')
+plt.ylabel('ESTIMATED SALARY')
+plt.legend()
+plt.show()
+
+#VISUALISING TEST SET RESULTS
+from matplotlib.colors import ListedColormap
+X_set,y_set=X_test,y_test
+X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
+                  np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75,cmap=ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j,0],X_set[y_set==j,1],
+                c=ListedColormap(('red','green'))(i),label=j)
+plt.title('K-NN test set')
+plt.xlabel('AGE')
+plt.ylabel('ESTIMATED SALARY')
+plt.legend()
+plt.show()
